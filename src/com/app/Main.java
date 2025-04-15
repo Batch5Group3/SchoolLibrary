2883cf22cafed7a97584e99c50e61f0aebc29dc7
cahizon <sj5.cahizon24@joysistvi.edu.ph>
2025-04-15 11:14:40 +0800

    Login feature

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -3,14 +3,36 @@ package com.app;
 
 import com.controller.BookController;
 import com.view.BookView;
-
+import com.view.LoginView;
+import java.util.Scanner;
+        
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         //System.out.println("Initial commit");
         //System.out.println("Initial commit from branch off...");
         // I think in this part... it's the dependency injection mechanism???
-        BookController bookController = new BookController();
-        BookView bookView = new BookView(bookController);
-        bookView.showMainMenu();
+       
+         LoginView loginView = new LoginView();
+         initial(loginView);
+    }
+    
+    public static void initial(LoginView loginView){
+        if(loginView.showLogin()){
+            BookController bookController = new BookController();
+            BookView bookView = new BookView(bookController);
+            bookView.showMainMenu();
+        } else {
+            System.out.print("Retry? Y/N : ");
+            Scanner sc = new Scanner(System.in);
+            String choice = sc.nextLine();
+            if(choice.toLowerCase().equals("y")){
+                System.out.println("Please Input Correct Credential");
+                initial(loginView);
+            }else{
+                System.out.println("Exiting Program...");
+                System.exit(0);
+            }
+        }
     }
 }
